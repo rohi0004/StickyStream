768de0aac94c711c6ece7b10faace169bd858892
Rohit kumar <87049122+rohi0004@users.noreply.github.com>
2024-06-22 12:46:48 +0530

    Update url index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ mongoose.connect(mongoURL, {
 
 app.use(cors(
     {
-        origin: ['sticky-stream-frontend-ek4usdk7r-rohit-kumars-projects-5e2437aa.vercel.app', 'http://localhost:3001'],
+        origin: ['https://frontend-ddk1ess41-rohit-kumars-projects-5e2437aa.vercel.app', 'http://localhost:3001'],
         credentials: true,
     }
 ));
