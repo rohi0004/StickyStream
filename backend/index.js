d7daa389b08f5252454d213f17e9ca41386ab05e
Rohit kumar <87049122+rohi0004@users.noreply.github.com>
2024-06-22 12:27:29 +0530

    Added frontend url index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ mongoose.connect(mongoURL, {
 
 app.use(cors(
     {
-        origin: ['https://frontend-drab-seven.vercel.app', 'http://localhost:3001'],
+        origin: ['sticky-stream-frontend-ek4usdk7r-rohit-kumars-projects-5e2437aa.vercel.app', 'http://localhost:3001'],
         credentials: true,
     }
 ));
